110328a6de3987fa3febf4f8c048edf40356e1a0
Marco Agostini <107429726+Marukoalitini@users.noreply.github.com>
2024-08-07 19:44:17 -0300

    Atividade nova

---
+++ AtividadeCarro/src/Carro.java
@@ -0,0 +1,23 @@
+public class Carro {
+    public String marca;
+    public String modelo;
+    public int ano;
+    public String cor;
+    public String placa;
+
+    public void ligar() {
+        System.out.println("Carro ligado");
+    }
+    public void desligar(){
+        System.out.println("Carro desligado");
+    }
+    public void acelerar(){
+        System.out.println("Carro acelerando");
+    }
+    public void frear(){
+        System.out.println("Carro freando");
+    }
+    public void trocarMarcha(){
+        System.out.println("Carro trocando marcha");
+    }
+}
